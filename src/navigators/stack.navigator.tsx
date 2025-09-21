2733551690d242bb9b15981268e957f3c953d1a9
Taha Muntazir <65804464+tahamuntazir@users.noreply.github.com>
2025-09-21 17:48:37 +0500

    Architecture done

--- src/navigators/stack.navigator.tsx
+++ src/navigators/stack.navigator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //Screens
-import Home from '../screens/App/Home/index';
+import Home from '../screens/App/Home';
 import { AppStackParamList } from '../types/navigation.types.ts';
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
