b42a84169f95af1934912ef5df10c3e27ff13c0c
Aarab Baloch <72593922+aarabbaloch79@users.noreply.github.com>
2026-02-09 15:08:38 +0500

    notification navigation set with enums..

--- src/screens/app/notifications/index.tsx
+++ src/screens/app/notifications/index.tsx
@@ -7,15 +7,20 @@ import { useLocaleStore } from '../../../store/reducer/locale';
 import HomeHeader from '../../../components/global/HomeHeader.tsx';
 import NotificationItem from '../../../components/global/NotificationItem.tsx';
 import PlaceholderLogoText from '../../../components/global/PlaceholderLogoText.tsx';
+import { NotificationType } from '../../../utils/notification_enums';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { AppStackParamList } from '../../../types/navigation.types.ts';
 
 const NotificationsScreen: React.FC = () => {
   const { styles, theme } = useStyles();
   const { getString } = useLocaleStore();
-  const navigation = useNavigation();
+  const navigation =
+  useNavigation<NativeStackNavigationProp<AppStackParamList>>();
 
   const mockNotifications = [
     {
       id: 1,
+      type: NotificationType.SpecialPriceMenu,
       title: `${getString(
         'NOT_NEW_FRAGRANCE_COLLECTION_AVAILABLE_AT',
       )} ${getString('FAV_MOCK_PERFUME_HOUSE')}`,
@@ -25,6 +30,7 @@ const NotificationsScreen: React.FC = () => {
     },
     {
       id: 2,
+      type: NotificationType.SpecialPriceMenu,
       title: `${getString('NOT_SPECIAL_OFFER_AT')} ${getString(
         'FAV_MOCK_GYM',
       )} - ${getString('NOT_LIMITED_TIME_ONLY')}`,
@@ -34,6 +40,7 @@ const NotificationsScreen: React.FC = () => {
     },
     {
       id: 3,
+      type: NotificationType.SpecialPriceMenu,
       title: `${getString('NOT_YOUR_SPECIAL_PRICE_MENU_AT')} ${getString(
         'FAV_MOCK_COFFEMATICS',
       )}`,
@@ -42,6 +49,62 @@ const NotificationsScreen: React.FC = () => {
       boldText: getString('FAV_MOCK_COFFEMATICS'),
     },
   ];
+
+ const handleNotificationPress = (notificationType: NotificationType) => {
+  switch (notificationType) {
+    case NotificationType.SupportRequest:
+      navigation.navigate('ContactUs');
+      break;
+
+    case NotificationType.AdminPanelNotification:
+      navigation.navigate('BottomTabs');
+      break;
+
+    case NotificationType.GiftReceived:
+     navigation.navigate('InboxOutbox', {
+            title: getString('HOME_INBOX'),
+            isInbox: true,
+          });
+      break;
+
+    case NotificationType.GiftRedeem:
+      navigation.navigate('Wallet');
+      break;
+
+    case NotificationType.SpecialPriceMenu:
+      navigation.navigate('StoreProducts', {});
+      break;
+
+    case NotificationType.AddFriend:
+      navigation.navigate('Profile');
+      break;
+
+    case NotificationType.Catch:
+      navigation.navigate('CatchScreen', {
+        type: 'catch',
+      });
+      break;
+
+    case NotificationType.G1G1:
+      navigation.navigate('GiftOneGetOne');
+      break;
+
+    case NotificationType.BirthdayNotification:
+      navigation.navigate('SendAGift', {
+        routeTo: 'SelectStore',
+      });
+      break;
+
+    case NotificationType.OccasionNotification:
+      navigation.navigate('Occasions',
+);
+      break;
+
+    default:
+      break;
+  }
+};
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -66,7 +129,7 @@ const NotificationsScreen: React.FC = () => {
         renderItem={({ item }: { item: (typeof mockNotifications)[0] }) => (
           <NotificationItem
             title={item.title}
-            onPress={() => { }}
+            onPress={() => handleNotificationPress(item.type)}
             NotificationItemStyles={styles.NotificationItem}
             isGroupImage={item.image}
             time={item.time}
---
+++ src/utils/notification_enums.ts
@@ -0,0 +1,12 @@
+export enum NotificationType {
+  SupportRequest = 1,
+  AdminPanelNotification = 2,
+  GiftReceived = 3,
+  GiftRedeem = 4,
+  SpecialPriceMenu = 5,
+  AddFriend = 6,
+  Catch = 7,
+  G1G1 = 8,
+  BirthdayNotification = 9,
+  OccasionNotification = 10,
+}
